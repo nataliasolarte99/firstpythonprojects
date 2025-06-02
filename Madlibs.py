de3e78b60054a8bdbd6025a7830bad8a9d83efb4
Natalia Solarte Rivera <nataliasolarterivera@gmail.com>
2025-06-02 16:07:44 +0200

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,20 @@
+#Madlibs game
+
+adjective1 = input("Enter and adjective:")
+noun1 = input("Enter a noun:")
+adjective2 = input("Enter and adjective:")
+verb1 = input("Enter a verb ending with 'ing':")
+adjective3 = input("Enter and adjective:")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun1}")
+print(f"{noun1} was {adjective2} and {verb1}" )
+print(f"I was {adjective3}")
+
+
+
+
+
+
+
+
